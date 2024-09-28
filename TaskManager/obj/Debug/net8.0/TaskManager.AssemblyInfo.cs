4ba76d018a18b05a36c48d56fde16ab213d4c58c
Malashree Dhungel <malashree.dhungel@coyotes.usd.edu>
2024-09-27 21:43:22 -0500

    changed alot of things. works properly

--- TaskManager/obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a643a178d51dfa262018b5b906830389e8d46c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2335188942cf3861b741a28a32246f21432ce05")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
